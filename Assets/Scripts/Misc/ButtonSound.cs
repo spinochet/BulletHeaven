876106081a2aa170ca0332a5903b6ba335c67646
Simon Pinochet <simon.pinochet@gmail.com>
2021-09-22 13:16:53 -0500

    Added UI sounds

---
+++ Assets/Scripts/Misc/ButtonSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ButtonSound : MonoBehaviour, ISelectHandler
+{
+    public AudioSource audio;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        audio.Play();
+    }
+
+    public void Select()
+    {
+        audio.Play();
+    }
+}
