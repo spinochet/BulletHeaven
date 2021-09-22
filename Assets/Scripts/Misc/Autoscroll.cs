411798db255d2c42608db8e1fb21efc71a70c57c
Simon Pinochet <simon.pinochet@gmail.com>
2021-09-22 13:42:56 -0500

    Improved whitebox level

---
+++ Assets/Scripts/Misc/Autoscroll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Autoscroll : MonoBehaviour
+{
+    [SerializeField] private float speed = 5.0f;
+
+    private float timer;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position -= Vector3.forward * speed * Time.deltaTime;
+        if (transform.position.z <= -137.5f)
+        {
+            speed = 0.0f;
+            timer += Time.deltaTime;
+
+            if (timer > 7.5f)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+        }
+    }
+}
